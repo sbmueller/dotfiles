752718813a72b703c4a5802a31a0797f1b6b801a
Sebastian Müller <sebastian.mueller3@zf.com>
2022-01-28 11:22:51 +0100

    Update nvim plugin configs

--- vim/.config/nvim/lua/treesitter-config.lua
+++ vim/.config/nvim/lua/treesitter-config.lua
@@ -1,16 +1,16 @@
-require'nvim-treesitter.configs'.setup {
-    ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-    ignore_install = {"javascript", "tlaplus"}, -- List of parsers to ignore installing
-    highlight = {
-        enable = true, -- false will disable the whole extension
-        disable = {} -- list of language that will be disabled
-    },
-    indent = {
-        enable = true, -- false will disable the whole extension
-        disable = {} -- list of language that will be disabled
-    },
-    folding = {
-        enable = true, -- false will disable the whole extension
-        disable = {} -- list of language that will be disabled
-    }
+require "nvim-treesitter.configs".setup {
+  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ignore_install = {"javascript", "tlaplus", "norg"}, -- List of parsers to ignore installing
+  highlight = {
+    enable = true, -- false will disable the whole extension
+    disable = {} -- list of language that will be disabled
+  },
+  indent = {
+    enable = true, -- false will disable the whole extension
+    disable = {} -- list of language that will be disabled
+  },
+  folding = {
+    enable = true, -- false will disable the whole extension
+    disable = {} -- list of language that will be disabled
+  }
 }
