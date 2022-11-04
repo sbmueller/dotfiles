2557af82f0de97c0faf41365d56dfb5f4ac22e98
Sebastian Müller <gsenpo@gmail.com>
2022-11-04 20:57:00 +0100

    Mac adaptions

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require "wezterm"
 return {
   term = "wezterm",
   font = wezterm.font("VictorMono Nerd Font", {weight = "Bold"}),
-  font_size = 11.0,
+  font_size = 17.0,
   hide_tab_bar_if_only_one_tab = true,
   color_scheme = "Catppuccin Macchiato",
   warn_about_missing_glyphs = false,
