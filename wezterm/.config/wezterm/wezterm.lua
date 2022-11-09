fbbcd74dab5470336e1c9c3ffc356bd6d9d577d0
Sebastian Müller <sebastian.mueller3@zf.com>
2022-11-09 13:28:15 +0100

    Wezterm: disable missing glyph warning

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -5,6 +5,7 @@ return {
   font_size = 11.0,
   hide_tab_bar_if_only_one_tab = true,
   color_scheme = "Catppuccin Macchiato",
+  warn_about_missing_glyphs = false,
   -- maximum real estate
   window_padding = {
     left = 0,
