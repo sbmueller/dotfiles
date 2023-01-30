78284c0f1d212cfa610f8fa0ab28c37d182b9664
Sebastian Müller <sebastian.mueller3@zf.com>
2023-01-30 13:32:55 +0100

    Update noice notifications

--- vim/.config/nvim/lua/noice-config.lua
+++ vim/.config/nvim/lua/noice-config.lua
@@ -1,5 +1,23 @@
+require("notify").setup(
+  {
+    render = "compact",
+    stages = "slide",
+    timeout = 1000
+  }
+)
+
 require("noice").setup(
   {
+    messages = {
+      -- NOTE: If you enable messages, then the cmdline is enabled automatically.
+      -- This is a current Neovim limitation.
+      enabled = true, -- enables the Noice messages UI
+      view = "notify", -- default view for messages
+      view_error = "notify", -- view for errors
+      view_warn = "notify", -- view for warnings
+      view_history = "messages", -- view for :messages
+      view_search = "virtualtext" -- view for search count messages. Set to `false` to disable
+    },
     lsp = {
       hover = {
         enabled = true
@@ -14,7 +32,11 @@ require("noice").setup(
       }
     },
     presets = {
-      lsp_doc_border = true -- add a border to hover docs and signature help
+      lsp_doc_border = true, -- add a border to hover docs and signature help
+      bottom_search = false, -- use a classic bottom cmdline for search
+      command_palette = false, -- position the cmdline and popupmenu together
+      long_message_to_split = true, -- long messages will be sent to a split
+      inc_rename = false -- enables an input dialog for inc-rename.nvim
     }
   }
 )
