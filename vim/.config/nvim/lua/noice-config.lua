6e4ee4690375c928770e57ec83710e437654f717
Sebastian Müller <sebastian.mueller3@zf.com>
2023-03-14 09:48:04 +0100

    Nvim: Fix spell correction in Noice

--- vim/.config/nvim/lua/noice-config.lua
+++ vim/.config/nvim/lua/noice-config.lua
@@ -35,7 +35,7 @@ require("noice").setup(
       lsp_doc_border = true, -- add a border to hover docs and signature help
       bottom_search = false, -- use a classic bottom cmdline for search
       command_palette = false, -- position the cmdline and popupmenu together
-      long_message_to_split = true, -- long messages will be sent to a split
+      long_message_to_split = false, -- long messages will be sent to a split
       inc_rename = false -- enables an input dialog for inc-rename.nvim
     }
   }
