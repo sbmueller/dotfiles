969efbf1458f049fd45def6a347ad21661b7aed7
Sebastian Müller <sebastian.mueller3@zf.com>
2023-08-15 12:19:31 +0200

    Nvim: Fix cmp key mappings in cmdline

--- vim/.config/nvim/lua/cmp-config.lua
+++ vim/.config/nvim/lua/cmp-config.lua
@@ -13,8 +13,8 @@ cmp.setup(
       end
     },
     mapping = {
-      ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), {"i", "s"}),
-      ["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), {"i", "s"}),
+      ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), {"i", "s", "c"}),
+      ["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), {"i", "s", "c"}),
       ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), {"i", "c"}),
       ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), {"i", "c"}),
       ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), {"i", "c"}),
