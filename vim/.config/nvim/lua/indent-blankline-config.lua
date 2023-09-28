331dfefd650fbea78f990d5233fe8c56445ed165
Sebastian Müller <sebastian.mueller3@zf.com>
2023-09-28 07:57:57 +0200

    Nvim: Update indent_blankline to v3

--- vim/.config/nvim/lua/indent-blankline-config.lua
+++ vim/.config/nvim/lua/indent-blankline-config.lua
@@ -1,5 +1,3 @@
-require("indent_blankline").setup {
-  buftype_exclude = {"terminal"},
-  filetype_exclude = {"dashboard"},
-  show_current_context = true
+require("ibl").setup {
+  exclude = {filetypes = {"dashboard"}}
 }
