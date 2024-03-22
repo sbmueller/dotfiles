db2de023ddf4bd1db6350202b2686d72265dccac
Sebastian Müller <sebastian.mueller3@zf.com>
2024-03-22 14:41:02 +0100

    Nvim: Add linting status to lualine

--- vim/.config/nvim/lua/plugins/lualine.lua
+++ vim/.config/nvim/lua/plugins/lualine.lua
@@ -2,40 +2,51 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
-    dependencies = {"nvim-lua/lsp-status.nvim"},
-    opts = {
-      options = {
-        icons_enabled = true,
-        theme = "auto",
-        component_separators = {left = "", right = ""},
-        section_separators = {left = "", right = ""},
-        disabled_filetypes = {},
-        always_divide_middle = true
-      },
-      sections = {
-        lualine_a = {"mode"},
-        lualine_b = {"branch", "diff"},
-        lualine_c = {"filename"},
-        lualine_x = {
-          "encoding",
-          "fileformat",
-          "filetype",
-          -- {"diagnostics", sources = {"nvim_diagnostic"}},
-          "require'lsp-status'.status()"
+    dependencies = {"nvim-lua/lsp-status.nvim", "mfussenegger/nvim-lint"},
+    config = function()
+      -- Linting progress helper function
+      local function lint_progress()
+        local linters = require("lint").get_running()
+        if #linters == 0 then
+          return "󰦕"
+        end
+        return "󱉶 " .. table.concat(linters, ", ")
+      end
+      require("lualine").setup {
+        options = {
+          icons_enabled = true,
+          theme = "auto",
+          component_separators = {left = "", right = ""},
+          section_separators = {left = "", right = ""},
+          disabled_filetypes = {},
+          always_divide_middle = true
         },
-        lualine_y = {"progress"},
-        lualine_z = {"location"}
-      },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = {"filename"},
-        lualine_x = {"location"},
-        lualine_y = {},
-        lualine_z = {}
-      },
-      tabline = {},
-      extensions = {}
-    }
+        sections = {
+          lualine_a = {"mode"},
+          lualine_b = {"branch", "diff"},
+          lualine_c = {"filename"},
+          lualine_x = {
+            "encoding",
+            "fileformat",
+            "filetype",
+            -- {"diagnostics", sources = {"nvim_diagnostic"}},
+            lint_progress,
+            "require'lsp-status'.status()"
+          },
+          lualine_y = {"progress"},
+          lualine_z = {"location"}
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = {"filename"},
+          lualine_x = {"location"},
+          lualine_y = {},
+          lualine_z = {}
+        },
+        tabline = {},
+        extensions = {}
+      }
+    end
   }
 }
