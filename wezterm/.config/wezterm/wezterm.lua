49c21466225b13c57903fa5cf62f4be386a808f3
Sebastian Müller <sebastian.mueller3@zf.com>
2024-06-12 11:04:05 +0200

    Wezterm: Enable transparency

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -4,6 +4,7 @@ return {
   font = wezterm.font("VictorMono Nerd Font", {weight = "Bold"}),
   font_size = 11.0,
   hide_tab_bar_if_only_one_tab = true,
+  window_background_opacity = 0.90,
   color_scheme = "Catppuccin Macchiato",
   warn_about_missing_glyphs = false,
   -- maximum real estate
