0378f1c15e4264167f9a9e8794b35f5700498cc6
Sebastian Müller <sebastian.mueller3@zf.com>
2024-06-12 11:03:58 +0200

    Nvim: Visual updates

--- vim/.config/nvim/lua/plugins/dashboard.lua
+++ vim/.config/nvim/lua/plugins/dashboard.lua
@@ -3,37 +3,53 @@ return {
     "glepnir/dashboard-nvim",
     event = "VimEnter",
     dependencies = {"nvim-tree/nvim-web-devicons"},
-    opts = {
-      theme = "hyper",
-      config = {
-        week_header = {
-          enable = true
-        },
-        packages = {enable = true},
-        shortcut = {
-          {desc = "󰊳 Update", group = "@property", action = "Lazy update", key = "u"},
-          {
-            icon = " ",
-            icon_hl = "@variable",
-            desc = "Files",
-            group = "Label",
-            action = "Telescope find_files",
-            key = "f"
-          },
-          {
-            desc = "󰴒 Journal",
-            group = "DiagnosticHint",
-            action = "ObsidianToday",
-            key = "j"
-          },
-          {
-            desc = " Notes",
-            group = "Number",
-            action = "ObsidianQuickSwitch",
-            key = "n"
+    config = function()
+      require("dashboard").setup(
+        {
+          theme = "hyper",
+          config = {
+            header = {
+              [[                                                                       ]],
+              [[                                                                     ]],
+              [[       ████ ██████           █████      ██                     ]],
+              [[      ███████████             █████                             ]],
+              [[      █████████ ███████████████████ ███   ███████████   ]],
+              [[     █████████  ███    █████████████ █████ ██████████████   ]],
+              [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+              [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+              [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+              [[                                                                       ]]
+            },
+            week_header = {
+              enable = false
+            },
+            packages = {enable = true},
+            shortcut = {
+              {desc = "󰊳 Update", group = "@property", action = "Lazy update", key = "u"},
+              {
+                icon = " ",
+                icon_hl = "@variable",
+                desc = "Files",
+                group = "Label",
+                action = "Telescope find_files",
+                key = "f"
+              },
+              {
+                desc = "󰴒 Journal",
+                group = "DiagnosticHint",
+                action = "ObsidianToday",
+                key = "j"
+              },
+              {
+                desc = " Notes",
+                group = "Number",
+                action = "ObsidianQuickSwitch",
+                key = "n"
+              }
+            }
           }
         }
-      }
-    }
+      )
+    end
   }
 }
--- vim/.config/nvim/lua/plugins/eldritch.lua
+++ vim/.config/nvim/lua/plugins/eldritch.lua
@@ -5,6 +5,7 @@ return {
     priority = 1000,
     config = function()
       -- load the colorscheme here
+      -- require("eldritch").setup({transparent = true})
       vim.cmd.colorscheme "eldritch"
     end
   }
