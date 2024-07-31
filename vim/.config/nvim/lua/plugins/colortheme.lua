46cd5172d13d083ef0ec45c75b0b9e56295d94d9
Sebastian Müller <gsenpo@gmail.com>
2024-07-31 10:23:22 +0200

    Nvim: Switch from eldritch to candyland colorscheme

--- vim/.config/nvim/lua/plugins/colortheme.lua
+++ vim/.config/nvim/lua/plugins/colortheme.lua
@@ -1,12 +1,12 @@
 return {
   {
-    "eldritch-theme/eldritch.nvim",
+    "AmberLehmann/candyland.nvim",
     lazy = false,
     priority = 1000,
     config = function()
       -- load the colorscheme here
       -- require("eldritch").setup({transparent = true})
-      vim.cmd.colorscheme "eldritch"
+      vim.cmd.colorscheme "candyland"
     end
   }
 }
