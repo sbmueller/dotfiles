df4ba4abd75ad36ee2a5f57ebd9de95b92c1de52
Sebastian Müller <gsenpo@gmail.com>
2024-10-18 09:00:28 +0200

    Nvim: Replace ltex-ls by harper-ls

--- vim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ vim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,45 +1,25 @@
 -- Enabler for inlay hints
 local on_attach = function(client, bufnr)
   if client.server_capabilities.inlayHintProvider then
-    vim.lsp.inlay_hint.enable(true, {bufnr = bufnr})
+    vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
   else
-    vim.lsp.inlay_hint.enable(false, {bufnr = bufnr})
+    vim.lsp.inlay_hint.enable(false, { bufnr = bufnr })
   end
   require("lsp-status").on_attach(client, bufnr)
 end
 
--- Helper to change ltex language during runtime
-vim.api.nvim_create_user_command(
-  "LtexChangeLanguage",
-  function(data)
-    local language = data.fargs[1]
-    local clients = vim.lsp.get_active_clients()
-    for _, client in ipairs(clients) do
-      if client.name == "ltex" then
-        client.config.settings.ltex.language = language
-        client.notify("workspace/didChangeConfiguration", {settings = client.config.settings})
-      end
-    end
-    print("LTeX language changed to " .. language)
-  end,
-  {
-    nargs = 1,
-    force = true
-  }
-)
-
 return {
   {
     "neovim/nvim-lspconfig",
-    dependencies = {"nvim-lua/lsp-status.nvim"},
-    ft = {"python", "cpp", "c", "rust", "cmake", "tex", "lua", "markdown"},
+    dependencies = { "nvim-lua/lsp-status.nvim" },
+    ft = { "python", "cpp", "c", "rust", "cmake", "tex", "lua", "markdown" },
     keys = {
-      {"<leader>d", vim.diagnostic.open_float, desc = "Line Diagnostics"},
-      {"<leader>D", vim.diagnostic.setloclist, desc = "Buffer Diagnostics"},
-      {"gD", vim.lsp.buf.declaration, desc = "Goto Declaration"},
-      {"K", vim.lsp.buf.hover, desc = "Hover Documentation"},
-      {"<leader>rn", vim.lsp.buf.rename, desc = "Rename Symbol"},
-      {"<leader>h", vim.lsp.buf.code_action, desc = "LSP Code Action"}
+      { "<leader>d",  vim.diagnostic.open_float, desc = "Line Diagnostics" },
+      { "<leader>D",  vim.diagnostic.setloclist, desc = "Buffer Diagnostics" },
+      { "gD",         vim.lsp.buf.declaration,   desc = "Goto Declaration" },
+      { "K",          vim.lsp.buf.hover,         desc = "Hover Documentation" },
+      { "<leader>rn", vim.lsp.buf.rename,        desc = "Rename Symbol" },
+      { "<leader>h",  vim.lsp.buf.code_action,   desc = "LSP Code Action" }
       -- gr: Show References is handled by Telescope
       -- gi: Show Implementations is handled by Telescope
       -- gd: Show Definitions is handled by Telescope
@@ -96,27 +76,16 @@ return {
       -- Tex
       lspconfig.texlab.setup {}
 
-      -- Languagetool
-      lspconfig.ltex.setup {
-        settings = {
-          ltex = {
-            additionalRules = {
-              enablePickyRules = true,
-              motherTongue = "de"
-            },
-            language = "en-US"
-          }
-        }
-      }
-
+      -- Harper
+      lspconfig.harper_ls.setup {}
       -- Lua
       lspconfig.lua_ls.setup {}
 
       -- Severity Signs
-      local signs = {Error = "", Warn = "", Hint = "", Info = "󰋼"}
+      local signs = { Error = "", Warn = "", Hint = "", Info = "󰋼" }
       for type, icon in pairs(signs) do
         local hl = "DiagnosticSign" .. type
-        vim.fn.sign_define(hl, {text = icon, texthl = hl, numhl = hl})
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
       end
     end
   }
