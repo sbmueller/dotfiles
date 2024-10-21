51e611c5f011eda945bbab7016b73727a3c12a9d
Sebastian Müller <gsenpo@gmail.com>
2024-10-21 13:39:46 +0200

    Nvim: Update c/c++ settings

--- vim/.config/nvim/after/ftplugin/c.lua
+++ vim/.config/nvim/after/ftplugin/c.lua
@@ -1,5 +1,5 @@
-vim.opt_local.ts = 4
-vim.opt_local.sw = 4
-vim.opt_local.tw = 140
+vim.opt_local.ts = 2
+vim.opt_local.sw = 2
+vim.opt_local.tw = 80
 vim.opt_local.expandtab = true
-vim.opt_local.colorcolumn = "140"
+vim.opt_local.colorcolumn = "80"
