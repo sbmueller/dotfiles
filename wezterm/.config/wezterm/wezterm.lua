32f624959747bc8a8f87954e8b678ca3ce7ef21f
Sebastian Müller <gsenpo@gmail.com>
2024-11-27 18:51:03 +0100

    Wezterm: Set theme ChallengerDeep

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,10 +1,10 @@
 local wezterm = require "wezterm"
 return {
   term = "wezterm",
-  font = wezterm.font("VictorMono Nerd Font", { weight = "Bold" }),
+  font = wezterm.font("VictorMono Nerd Font", {weight = "Bold"}),
   font_size = 17.0,
   hide_tab_bar_if_only_one_tab = true,
-  color_scheme = "Catppuccin Macchiato",
+  color_scheme = "ChallengerDeep",
   warn_about_missing_glyphs = false,
   -- maximum real estate
   window_padding = {
