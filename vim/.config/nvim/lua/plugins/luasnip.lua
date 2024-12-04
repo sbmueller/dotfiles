3cc14e58bcd5e430a322e6706ceb3178c26b3606
Sebastian Müller <gsenpo@gmail.com>
2024-12-04 18:04:58 +0100

    Nvim: Create new snippets file

--- vim/.config/nvim/lua/plugins/luasnip.lua
+++ vim/.config/nvim/lua/plugins/luasnip.lua
@@ -8,7 +8,7 @@ return {
     config = function()
       local luasnip_loader = require("luasnip.loaders.from_vscode")
       luasnip_loader.lazy_load() -- Snippets in path
-      luasnip_loader.lazy_load({paths = {"~/dotfiles/zf"}}) -- Custom ZF snippets
+      luasnip_loader.lazy_load({paths = {"~/snippets"}}) -- Custom ZF snippets
     end
   }
 }
