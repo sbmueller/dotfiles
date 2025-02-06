dc2f61c36ad41334f4728775a27c6c4d5ca794d6
Sebastian Müller <gsenpo@gmail.com>
2025-02-06 10:50:02 +0100

    Nvim: Format config

--- vim/.config/nvim/lua/plugins/bufferline.lua
+++ vim/.config/nvim/lua/plugins/bufferline.lua
@@ -1,23 +1,23 @@
 return {
-  {
-    "akinsho/bufferline.nvim",
-    dependencies = "nvim-tree/nvim-web-devicons",
-    opts = {
-      options = {
-        numbers = "buffer_id",
-        max_name_length = 36,
-        max_prefix_length = 16,
-        tab_size = 22,
-        diagnostics = "nvim_lsp",
-        show_buffer_close_icons = false,
-        show_close_icon = false,
-        separator_style = "slant"
-      },
-      highlights = {
-        buffer_selected = {
-          italic = true
+    {
+        "akinsho/bufferline.nvim",
+        dependencies = "nvim-tree/nvim-web-devicons",
+        opts = {
+            options = {
+                numbers = "buffer_id",
+                max_name_length = 36,
+                max_prefix_length = 16,
+                tab_size = 22,
+                diagnostics = "nvim_lsp",
+                show_buffer_close_icons = false,
+                show_close_icon = false,
+                separator_style = "slant"
+            },
+            highlights = {
+                buffer_selected = {
+                    italic = true
+                }
+            }
         }
-      }
     }
-  }
 }
