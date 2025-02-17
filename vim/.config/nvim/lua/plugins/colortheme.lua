7b6a7b09291756d32783b13233aa6d39a97d4d70
Sebastian Müller <gsenpo@gmail.com>
2025-02-17 18:19:38 +0100

    Nvim: Switch to catppuccin theme

--- vim/.config/nvim/lua/plugins/colortheme.lua
+++ vim/.config/nvim/lua/plugins/colortheme.lua
@@ -1,12 +1,12 @@
 return {
   {
-    "eldritch-theme/eldritch.nvim",
-    lazy = false,
+    "catppuccin/nvim",
+    name = "catppuccin",
     priority = 1000,
     config = function()
       -- load the colorscheme here
       -- require("eldritch").setup({transparent = true})
-      vim.cmd.colorscheme "eldritch"
+      vim.cmd.colorscheme "catppuccin-macchiato"
     end
   }
 }
