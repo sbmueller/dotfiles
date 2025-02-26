480ef74faa680d322e2c5baea73ca1277a6cc790
Sebastian Müller <gsenpo@gmail.com>
2025-02-26 18:02:53 +0100

    Nvim: Fix markdown plugins

--- vim/.config/nvim/lua/plugins/markview.lua
+++ vim/.config/nvim/lua/plugins/markview.lua
@@ -1,17 +1,10 @@
 return {
   "OXY2DEV/markview.nvim",
-  lazy = true,
-  ft = {"markdown", "rmarkdown", "quarto", "Avante"},
-  dependencies = {
-    -- You may not need this if you don't lazy load
-    -- Or if the parsers are in your $RUNTIMEPATH
-    "nvim-treesitter/nvim-treesitter",
-    "nvim-tree/nvim-web-devicons"
-  },
+  lazy = false,
   opts = {
     preview = {
       enable = true,
-      filetypes = {"md", "rmd", "quarto", "Avante"}
+      filetypes = {"markdown", "rmd", "quarto", "Avante"}
     }
   }
 }
