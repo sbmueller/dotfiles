8fce9e58fadca50876cc78a49269c227e4e6c00e
Sebastian Müller <gsenpo@gmail.com>
2025-03-27 17:24:18 +0100

    Nvim: Use claude-3.7

--- vim/.config/nvim/lua/plugins/avante.lua
+++ vim/.config/nvim/lua/plugins/avante.lua
@@ -1,35 +1,35 @@
 return {
-    {
-        "yetone/avante.nvim",
-        lazy = true,
-        cmd = "AvanteAsk",
-        keys = {"<leader>aa", "<leader>ae"},
-        version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
-        opts = {
-            -- add any opts here
-            -- for example
-            provider = "copilot",
-            copilot = {
-                endpoint = "https://api.githubcopilot.com",
-                model = "o3-mini",
-                allow_insecure = false, -- Allow insecure server connections
-                timeout = 30000, -- Timeout in milliseconds
-                temperature = 0,
-                max_tokens = 4096
-            }
-        },
-        -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-        build = "make",
-        -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-        dependencies = {
-            "stevearc/dressing.nvim",
-            "nvim-lua/plenary.nvim",
-            "MunifTanjim/nui.nvim",
-            --- The below dependencies are optional,
-            "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-            "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-            "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-            "zbirenbaum/copilot.lua" -- for providers='copilot'
-        }
+  {
+    "yetone/avante.nvim",
+    lazy = true,
+    event = "VeryLazy",
+    version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+    opts = {
+      -- add any opts here
+      -- for example
+      provider = "copilot",
+      copilot = {
+        endpoint = "https://api.githubcopilot.com",
+        model = "claude-3.7-sonnet",
+        -- model = "o3-mini",
+        allow_insecure = false, -- Allow insecure server connections
+        timeout = 30000, -- Timeout in milliseconds
+        temperature = 0,
+        max_tokens = 4096
+      }
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = "make",
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+      "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua" -- for providers='copilot'
     }
+  }
 }
