3b8519c1f486db2f80c7ab5ab8278101cab671cf
Sebastian Müller <gsenpo@gmail.com>
2025-05-08 15:30:16 +0200

    Visual improvements

--- vim/.config/nvim/lua/neovide.lua
+++ vim/.config/nvim/lua/neovide.lua
@@ -2,7 +2,7 @@ vim.g.neovide_cursor_vfx_mode = "ripple"
 vim.g.neovide_cursor_animate_in_insert_mode = true
 
 -- g:neovide_opacity should be 0 if you want to unify transparency of content and title bar.
-vim.g.neovide_opacity = 0.8
+vim.g.neovide_opacity = 0.9
 vim.g.neovide_window_blurred = true
 -- Set a background color even for transparent background
 vim.api.nvim_set_hl(0, "Normal", { bg = "#1b182c" }) -- Set your desired transparent color here
