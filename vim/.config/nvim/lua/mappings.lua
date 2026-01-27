40543f4a222206726802bf74e28670a628907248
Sebastian Müller <gsenpo@gmail.com>
2026-01-27 14:53:27 +0100

    Nvim: Fix tabbing out of toggleterm

--- vim/.config/nvim/lua/mappings.lua
+++ vim/.config/nvim/lua/mappings.lua
@@ -4,26 +4,26 @@ local wk = require("which-key")
 
 wk.add(
   {
-    {"n", "nzz", desc = "Next occurence and center"},
-    {"N", "Nzz", desc = "Previous occurence and center"},
-    {"<space>", "<C-W><C-W>", desc = "Next Window"},
-    {"<tab>", "<cmd>bn<cr>", desc = "Next Buffer"},
-    {"<S-tab>", "<cmd>bp<cr>", desc = "Previous Buffer"},
-    {"<leader>RN", ":%s/<C-r><C-w>//g<Left><Left>", desc = "Replace word globally"}
+    { "n",          "nzz",                           desc = "Next occurence and center" },
+    { "N",          "Nzz",                           desc = "Previous occurence and center" },
+    { "<space>",    "<C-W><C-W>",                    desc = "Next Window" },
+    { "<tab>",      "<cmd>bn<cr>",                   desc = "Next Buffer",                  mode = "n" },
+    { "<S-tab>",    "<cmd>bp<cr>",                   desc = "Previous Buffer",              mode = "n" },
+    { "<leader>RN", ":%s/<C-r><C-w>//g<Left><Left>", desc = "Replace word globally" }
   }
 )
 
 --Autoformat Toggle
 wk.add(
   {
-    {"<leader>a", "<cmd>FormatToggle<CR>", desc = "Toggle Autoformat"}
+    { "<leader>a", "<cmd>FormatToggle<CR>", desc = "Toggle Autoformat" }
   }
 )
 
 --Terminal Mappings
 wk.add(
   {
-    {"<Esc>", "<C-\\><C-n>", desc = "Leave Terminal Mode", noremap = true, mode = "t"}
+    { "<Esc>", "<C-\\><C-n>", desc = "Leave Terminal Mode", noremap = true, mode = "t" }
     --["<leader>t"] = {":vsplit term://zsh<CR>", "Open Terminal", noremap = true, mode = "n"}
   }
 )
