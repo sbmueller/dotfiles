b7cfed6b5ff37dd02816e87038eb94f92025a450
Sebastian Müller <gsenpo@gmail.com>
2026-02-17 10:12:38 +0100

    Nvim: Replace deprecated outline plugin

---
+++ vim/.config/nvim/lua/plugins/outline.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "hedyhli/outline.nvim",
+    lazy = true,
+    cmd = {"Outline", "OutlineOpen"},
+    keys = {
+      -- Example mapping to toggle outline
+      {"<leader>s", "<cmd>Outline<CR>", desc = "Toggle outline"}
+    },
+    opts = {}
+  }
+}
